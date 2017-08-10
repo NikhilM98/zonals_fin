16c7c7797fd23f0b0ffda579637de7133e494a7c
Gaurav <gaurav.kumar9825@gmail.com>
2017-08-11 00:27:03 +0530

    tried to fix keyboard problem

--- js/form.js
+++ js/form.js
@@ -43,6 +43,9 @@ window.addEventListener("load",function() {
 });
 
 
-function onResize(){
-      $('body').css("height", page_height);
-  }
+
+
+var isAndroid = navigator.userAgent.toLowerCase().indexOf("android") > -1; //&& ua.indexOf("mobile");
+if(isAndroid) {
+    document.write('<meta name="viewport" content="width=device-width,height='+window.innerHeight+', initial-scale=1.0">');
+}
